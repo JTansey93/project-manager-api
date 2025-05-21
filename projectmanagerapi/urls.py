1b5ca336d496c0b2bf788f198e40cd011bc95444
James Tansey <57265750+JTansey93@users.noreply.github.com>
2025-05-21 12:21:54 +0100

    Added the admin sit eback to the main site urls config

--- projectmanagerapi/urls.py
+++ projectmanagerapi/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
         path('', include('kanban.urls')),
+        path('admin/', admin.site.urls),
         ]
